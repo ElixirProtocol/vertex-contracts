72d4d31ab2ece8bbdbeffb565df34b41a29403b0
cairo <101215230+cairoeth@users.noreply.github.com>
2023-06-29 20:50:06 -0400

    ♻️ Improved approach to factories

--- src/VertexFactory.sol
+++ src/VertexFactory.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.15;
+pragma solidity 0.8.19;
 
-import {VertexPool} from "./VertexPool.sol";
+import {VertexPool, ERC20} from "./VertexPool.sol";
+import {Bytes32AddressLib} from "solmate/utils/Bytes32AddressLib.sol";
+import {Owned} from "solmate/auth/Owned.sol";
 
 /// @title Elixir Pool Factory for Vertex
 /// @author The Elixir Team
 /// @notice Factory for Elixir-based Vertex pools based on a pair of ERC20 tokens.
-contract VaultFactory {
+contract VertexFactory is Owned {
     using Bytes32AddressLib for address;
     using Bytes32AddressLib for bytes32;
 
@@ -17,6 +19,12 @@ contract VaultFactory {
     /// @notice Returns the pool address for a given pair of tokens and a fee, or address 0 if it does not exist
     /// @dev tokenA and tokenB may be passed in either token0/token1 or token1/token0 order
     mapping(address => mapping(address => mapping(uint24 => address))) public getPool;
+
+    struct Product {
+        uint8 id;
+        address token0;
+        address token1;
+    }
 
     /*//////////////////////////////////////////////////////////////
                                  EVENTS
@@ -36,23 +44,36 @@ contract VaultFactory {
     error TokenIsZero();
 
     /*//////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    constructor() Owned(msg.sender) {}
+
+    /*//////////////////////////////////////////////////////////////
                            EXTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
     /// @notice Deploys a new Vault which supports a specific underlying token.
     /// @dev This will revert if a Vault that accepts the same underlying token has already been deployed.
+    /// @param id The ID of the pool on Vertex
     /// @param tokenA One of the two tokens in the desired pool
     /// @param tokenB The other of the two tokens in the desired pool
-    function deployVault(address tokenA, address tokenB) external returns (Vault vault) {
+    function deployVault(uint8 id, address tokenA, address tokenB) external onlyOwner returns (address pool) {
         if (tokenA == tokenB) revert SameTokens();
-        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        if (token0 == address(0)) revert TokenIsZero();
+        if (address(token0) == address(0)) revert TokenIsZero();
 
         // Use the CREATE2 opcode to deploy a new Vault contract.
         // This will revert if a Vault which accepts this underlying token has already
         // been deployed, as the salt would be the same and we can't deploy with it twice.
-        pool = address(new VertexPool{salt: keccak256(abi.encode(token0, token1))}(token0, token1));
+        pool = address(
+            new VertexPool{salt: keccak256(abi.encode(token0, token1))}(
+                string(abi.encodePacked("Elixir LP ", token0.name(), "-", token1.name(), " for Vertex")),
+                string(abi.encodePacked("elxr-", token0.symbol(), "-", token1.symbol())),
+                token0,
+                token1
+            )
+        );
 
-        emit PoolDeployed(token0, token1, pool);
+        emit PoolDeployed(address(token0), address(token1), pool);
     }
 }

c5a4c6f4db401024d75df84e3644eb8c25c0966e
cairo <cairoeth@protonmail.com>
2024-09-11 22:56:23 +0200

    Add to script

--- script/Upgrade.s.sol
+++ script/Upgrade.s.sol
@@ -29,6 +29,40 @@ contract UpgradeContract is Script {
 
         // Check upgrade by ensuring storage is not changed.
         require(address(manager.endpoint()) == endpoint, "Invalid upgrade");
+
+        uint256[] memory ids = new uint256[](6);
+        ids[0] = 31;
+        ids[1] = 31;
+        ids[2] = 31;
+        ids[3] = 31;
+        ids[4] = 31;
+        ids[5] = 31;
+
+        address[] memory tokens = new address[](6);
+        tokens[0] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+        tokens[1] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+        tokens[2] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+        tokens[3] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+        tokens[4] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+        tokens[5] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+
+        uint256[] memory shares = new uint256[](6);
+        shares[0] = 2603188792;
+        shares[1] = 2526477687;
+        shares[2] = 8254481486;
+        shares[3] = 8232673374;
+        shares[4] = 8254498436;
+        shares[5] = 8254430636;
+
+        address[] memory users = new address[](6);
+        users[0] = 0xc891b42f7E4753DB9B0B428Ec971B6a37Bc3Df5E;
+        users[1] = 0x3A0cd76779c934b349FaA92bC9A9f7f522557687;
+        users[2] = 0xA6012Fd4cD7e44c6bf606039D54260F30242D152;
+        users[3] = 0x207E3590513855CF88492DD943141216311767b9;
+        users[4] = 0x67Dac23B044054D498cE04A03AB8473F49b9a66d;
+        users[5] = 0x51b1A0310FC2bF7d1694Cf1f3feEcAC84bFfbc37;
+
+        manager.updateShares(ids, tokens, shares, users);
     }
 
     // Exclude from coverage report
--- test/VertexManagerUpgrade.t.sol
+++ test/VertexManagerUpgrade.t.sol
@@ -33,63 +33,54 @@ contract TestVertexManagerUpgrade is Test, ProcessQueue {
         // Deploy with key.
         vm.startPrank(manager.owner());
 
+        // Wrap in ABI to support easier calls.
+        manager = VertexManager(0x052Ab3fd33cADF9D9f227254252da3f996431f75);
+
         // Get the endpoint address before upgrading.
         address endpoint = address(manager.endpoint());
 
-        // Deploy new Processor implementation.
-        VertexProcessor newProcessor = new VertexProcessor();
-
-        // Deploy new Manager implementation.
+        // Deploy new implementation.
         VertexManager newManager = new VertexManager();
 
         // Upgrade proxy to new implementation.
-        manager.upgradeToAndCall(
-            address(newManager), abi.encodeWithSelector(VertexManager.updateProcessor.selector, address(newProcessor))
-        );
+        manager.upgradeTo(address(newManager));
 
         // Check upgrade by ensuring storage is not changed.
         require(address(manager.endpoint()) == endpoint, "Invalid upgrade");
 
-        // Increase hardcap
-        uint256[] memory hardcaps = new uint256[](2);
-        hardcaps[0] = type(uint256).max;
-        hardcaps[1] = type(uint256).max;
+        uint256[] memory ids = new uint256[](6);
+        ids[0] = 31;
+        ids[1] = 31;
+        ids[2] = 31;
+        ids[3] = 31;
+        ids[4] = 31;
+        ids[5] = 31;
 
-        address[] memory spotTokens = new address[](2);
-        spotTokens[0] = address(BTC);
-        spotTokens[1] = address(USDC);
+        address[] memory tokens = new address[](6);
+        tokens[0] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+        tokens[1] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+        tokens[2] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+        tokens[3] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+        tokens[4] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+        tokens[5] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
 
-        manager.updatePoolHardcaps(1, spotTokens, hardcaps);
+        uint256[] memory shares = new uint256[](6);
+        shares[0] = 2603188792;
+        shares[1] = 2526477687;
+        shares[2] = 8254481486;
+        shares[3] = 8232673374;
+        shares[4] = 8254498436;
+        shares[5] = 8254430636;
 
-        vm.stopPrank();
+        address[] memory users = new address[](6);
+        users[0] = 0xc891b42f7E4753DB9B0B428Ec971B6a37Bc3Df5E;
+        users[1] = 0x3A0cd76779c934b349FaA92bC9A9f7f522557687;
+        users[2] = 0xA6012Fd4cD7e44c6bf606039D54260F30242D152;
+        users[3] = 0x207E3590513855CF88492DD943141216311767b9;
+        users[4] = 0x67Dac23B044054D498cE04A03AB8473F49b9a66d;
+        users[5] = 0x51b1A0310FC2bF7d1694Cf1f3feEcAC84bFfbc37;
 
-        uint256 amountBTC = 10 * 10 ** 8;
-        uint256 amountUSDC = manager.getBalancedAmount(address(BTC), address(USDC), amountBTC);
-
-        deal(address(BTC), address(this), amountBTC + manager.getTransactionFee(address(BTC)));
-        deal(address(USDC), address(this), amountUSDC);
-
-        BTC.approve(address(manager), amountBTC + manager.getTransactionFee(address(BTC)));
-        USDC.approve(address(manager), amountUSDC);
-
-        uint256 fee = manager.getTransactionFee(address(WETH));
-
-        manager.depositSpot{value: fee}(
-            1, address(BTC), address(USDC), amountBTC, amountUSDC, amountUSDC, address(this)
-        );
-
-        // Get the router address
-        (address router,,,) = manager.getPoolToken(1, address(BTC));
-
-        vm.startPrank(address(uint160(bytes20(VertexRouter(router).externalSubaccount()))));
-        processQueue(manager);
-        vm.stopPrank();
-
-        uint256 userActiveAmountBTC = manager.getUserActiveAmount(1, address(BTC), address(this));
-        uint256 userActiveAmountUSDC = manager.getUserActiveAmount(1, address(USDC), address(this));
-
-        assertEq(userActiveAmountBTC, amountBTC);
-        assertEq(userActiveAmountUSDC, amountUSDC);
+        manager.updateShares(ids, tokens, shares, users);
     }
 
     // Exclude from coverage report
